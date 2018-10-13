909ec0274e8368c1242ae6e9ca69ca827e2d35fb
nhat220801 <nhat220801@gmail.com>
2018-10-13 22:10:53 +0700

    hihihhaha

--- asasd.cpp
+++
@@ -1,5 +0,0 @@
-this is my first time 
-i dont know how to do this
-
-
-#this is so interesting jajajsdjasdjasd
---
+++ thu.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n;
+main()
+{
+    freopen("thu.inp","r",stdin);
+    freopen("thu.out","w",stdout);
+  // cin>>n;
+  //  for(int i=1;i<=n;i++) cout<<"n";
+    cin>>n;
+    cout<<"chao3";
+}
